12005c81be134baf6744b99be6cd751b0cfe876e
Mar19845 <54874288+Mar19845@users.noreply.github.com>
2020-02-23 23:21:48 -0600

    ...
    ....

---
+++ app/src/main/java/com/example/lab5/Inicio.kt
@@ -0,0 +1,24 @@
+package com.example.lab5
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+
+
+
+class Inicio : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater,
+            R.layout.fragment_inicio,container,false)
+        return binding.root
+        //return inflater.inflate(R.layout.fragment_inicio, container, false)
+    }
+}
--- app/src/main/java/com/example/lab5/MainActivity.kt
+++ app/src/main/java/com/example/lab5/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.lab5
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.lab5.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var binding: ActivityMainBinding
+    val Manager = supportFragmentManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
