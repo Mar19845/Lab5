87788cfd3d216d855226ac6421be10544b687f98
Mar19845 <54874288+Mar19845@users.noreply.github.com>
2020-02-23 23:32:32 -0600

    ...

--- app/src/main/java/com/example/lab5/Inicio.kt
+++ app/src/main/java/com/example/lab5/Inicio.kt
@@ -7,18 +7,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-
-
+import com.example.lab5.databinding.FragmentInicioBinding
 
 class Inicio : Fragment() {
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater,
+        //return inflater.inflate(R.layout.fragment_inicio, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val binding = DataBindingUtil.inflate<FragmentInicioBinding>(inflater,
             R.layout.fragment_inicio,container,false)
         return binding.root
-        //return inflater.inflate(R.layout.fragment_inicio, container, false)
     }
 }
+
+
